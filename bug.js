44f0e5950cb9ebd85de8134c227c1f5e4351b8b0
BugHunter-X <askzenai@gmail.com>
2025-01-21 07:44:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,27 @@
+```javascript
+const fs = require('fs');
+
+async function readFileAsync(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+async function processFile() {
+  try {
+    const data = await readFileAsync('./my-file.txt');
+    // Process the data
+    console.log(data);
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
+
+processFile();
+```
